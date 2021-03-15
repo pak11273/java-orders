d5a48303a7509aabe4134a4d89f5b6e3d893271c
pak11273 <pak11273@gmail.com>
2021-03-15 15:56:12 -0500

    adds spring framework, maven, h2, and jpa

---
+++ src/test/java/com/lambdaschool/javaordersmaster/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javaordersmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
